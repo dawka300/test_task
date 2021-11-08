8c9f9029c0a1b7364c01ef310fc38170cab462e2
dwesolowski <dawid.wesolowski@verestro.com>
2021-11-08 03:26:42 +0100

    configurations

---
+++ api/tests/UnitTest/TestCaseProphecy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\UnitTest;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class TestCaseProphecy extends TestCase
+{
+    use ProphecyTrait;
+}
